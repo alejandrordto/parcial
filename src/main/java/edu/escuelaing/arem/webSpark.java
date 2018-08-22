d8a91bdb9223d471425356ff7891da046494b362
alejandrordto <masterale2011@gmail.com>
2018-08-21 22:08:39 -0500

    subida

---
+++ src/main/java/edu/escuelaing/arem/webSpark.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arem;
+import static spark.Spark.*;
+/**
+ *
+ * @author Alejandro Rodriguez  
+ */
+public class webSpark {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello Heroku");
+ }
+    static int getPort() {
+    if (System.getenv("PORT") != null) {
+        return Integer.parseInt(System.getenv("PORT"));
+    }
+        return 4567; 
+    }
+}
