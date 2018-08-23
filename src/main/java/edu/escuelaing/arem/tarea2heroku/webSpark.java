5e30df688514f6c8443bea5df67e327c8c83ae93
alejandrordto <die.9511@gmail.com>
2018-08-23 12:58:56 -0500

    html

--- src/main/java/edu/escuelaing/arem/tarea2heroku/webSpark.java
+++ src/main/java/edu/escuelaing/arem/tarea2heroku/webSpark.java
@@ -14,10 +14,23 @@ public class webSpark {
     public static void main(String[] args) {
         
         app=new Calculadora();
-        get("/Appesti", (req, res) ->{ 
-                String lista= req.queryParams("value");
-                return calculo("?value");
-                });
+        get("/Appesti", (req, res) ->   ("<!DOCTYPE html>"+
+                                        "<html>"+
+                                        "<head>"+
+                                        "<title>Calculador de estadisticos</title>"+
+                                        "<meta charset='UTF-8'>"+
+                                        "</head>"+
+                                        "<body>"+
+                                        "<p>Ingrese la lista deseada, separe cada numero con una coma </p>"+
+                                        "<div>"+
+                                        "<label for='tnum'>Lista de numeros</label>"+
+                                        "<input type='text' id='list' name='lista'>"+
+                                        "<label for='lname'>Descubra los estadigrafos</label>"+
+                                        "<input type='text' id='esta' name='respuesta'>"+
+                                        "<button id='Butom' class='button'>Calcular</button>"+
+                                        "</div>"+
+                                        "</body>"+
+                                        "</html>"));
  }
     static int getPort() {
     if (System.getenv("PORT") != null) {
